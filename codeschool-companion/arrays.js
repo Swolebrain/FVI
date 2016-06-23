8e2152b9de571594b995e359eecdb18768f41d75
Swolebrain <thecodingteacher@gmail.com>
2016-06-22 20:38:25 -0400

    forgot to save

--- codeschool-companion/arrays.js
+++ codeschool-companion/arrays.js
@@ -7,3 +7,15 @@ function sumArr(arr){
   }
   return sum;
 }
+
+
+/* write a function which receives an array of numbers and
+returns the average of all the numbers */
+function avgArr(arr){
+  if (ar.length === 0) return 0;
+  var sum = 0;
+  for (var i =0; i < arr.length; i++){
+    sum += arr[i];
+  }
+  return sum/arr.length;
+} 
--- codeschool-companion/functions.js
+++ codeschool-companion/functions.js
@@ -32,3 +32,72 @@ function(){
   }
   console.log(x);
 }
+
+/*
+Given 2 positive int values, return the larger value that is in
+the range 10..20 inclusive, or return 0 if neither is in that range.
+*/
+
+function max1020(a,b){
+  if ( (a > b && isbw1020(a)) ) return a;
+  else if (b > a && isbw1020(b) ) return b;
+  else if ( a > b && !isbw1020(a) && isbw1020(b)) return b;
+  else if (b > a && !isbw1020(b) && isbw1020(a)) return a;
+  return 0;
+
+  function isbw1020(x){
+    return x >= 10 && x <= 20;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+if ( _____ ){
+
+} else if ( _____ ){
+
+} else if ( _____ ){
+
+}else{
+
+}
+
+
+
+for (var i = ____; i < _____; i++){
+  //do something with i
+}
+
+
+function(a,b,....c){
+  //function code
+}
+
+
+/* Arrays */
+//creation:
+var myArray = [1,2,3,4,5];
+//access:
+var x = myArray[3];
+//assignment to a cell:
+myArray[4] = 10; //[1,2,3,4,10]
+//get length
+myArray.length //5
+//remove last element
+var y = myArray.pop(); // y = 10; myArray = [1,2,3,4]
+//add elements
+myArray.push(90); //myArray = [1,2,3,4,90]
+//delete an elements
+delete myArray[2]; // myArray = [1,2,undefined,4,90]
+//iterate through array
+for (var i = 0; i <myArray.length; i++){
+  //do something with arr[i]
+}
